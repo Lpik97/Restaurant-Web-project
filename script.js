d1657fec1bbefb9ae025616faee79e2e46f07386
Lpik97 <Lpik97@users.noreply.github.com>
2023-03-30 20:53:40 +0200

    The image loco was selected by a querySelector and a new function called reloadPage will reload the entire webpage everytime the person clicks on the logo.

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
 }
 
+const logoImage = document.querySelector(".image-logo");
+function reloadPage () {
+    window.location.reload();
+}
+
