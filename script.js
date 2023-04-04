8a0ee567dbfb21da18286cc8532553ef5629a579
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-04 10:26:51 +0200

    The constants selecting the menu-bar, logo-image and navigation-bar are now on top of the file, to be executed first. The event listener that assignated a new class to the menuBar now does the same to its children elements.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const aboveHeaderText = document.querySelector(".above-header-text");
+const menuBar = document.querySelector('.menu-bar');
+const logoImage = document.querySelector('.logo-image');
+const navBar = document.querySelector('.navigation-bar');
+
 function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
 }
 
-const logoImage = document.querySelector(".image-logo");
 function reloadPage () {
     window.location.reload();
 }
 
-const menuBar = document.querySelector(".menu-bar");
-
 document.addEventListener('scroll', function () {
     if (window.scrollY > 0) {
         menuBar.classList.add('scrolledBar');
+        logoImage.classList.add('scrolledImage');
+        navBar.classList.add('scrolledNavBar');
     } else {
         menuBar.classList.remove('scrolledBar');
+        logoImage.classList.remove('scrolledImage');
+        navBar.classList.remove('scrolledNavBar');
     }
 });
