3d9ff8bb3ddf737d9d98ec1939f22e3a773731d3
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-08 09:38:37 +0200

    The carousel buttons were selected to use a forEach loop, adding an evenet listener to them which will wait for a click. Once that happens, a constant named offset will be created assigning the value of 1 to the button with the data-set of carousel-button = right, if not, -1. Another constant named slides will be created, selecting the elements with the data attribute of data-slides. Lastly, a constant named activeSlide will search for the slide with the data-attribue of data-active. A new variable named newIndex will go into the slides array, select their children (li elements) and sum the offset to the activeSlide. An if conditional will check wether the newIndex is less than 0, assigning to it the result of the li elements.length - 1. Another conditional will check if newIndex is greater or equal to the li elements.length and if true, assign a new value of 0. Lastly, the slide with the newIndex number will be assigned the data-attribute of active, and it will be removed from the one that had it in the past.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBar = document.querySelector('.menu-bar');
 const logoImage = document.querySelector('.logo-image');
 const navBar = document.querySelector('.navigation-bar');
+const carouselButtons = document.querySelectorAll("[data-carousel-button]");
 
 function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
@@ -21,3 +22,17 @@ document.addEventListener('scroll', function () {
         navBar.classList.remove('scrolledNavBar');
     }
 });
+
+carouselButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === 'right' ? 1 : -1;
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+        const activeSlide = slides.querySelector('[data-active]');
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    })
+})
