3ce10d442fdedc904a54efc2d83ab51bad4be428
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-27 20:42:51 +0200

    The functions related to the carousel were deleted from the script file.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const navBar = document.querySelector('.navbar');
 const navBarLogo = document.querySelector('.navbar-logo');
 const navBarList = document.querySelector('.navbar-list');
 const hero = document.querySelector('.hero');
-const carouselButtons = document.querySelectorAll("[data-carousel-button]");
 
 function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
@@ -25,17 +24,3 @@ document.addEventListener('scroll', function () {
         hero.classList.remove('scrolled-hero');
     }
 });
-
-carouselButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton === 'right' ? 1 : -1;
-        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
-        const activeSlide = slides.querySelector('[data-active]');
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0) newIndex = slides.children.length - 1;
-        if (newIndex >= slides.children.length) newIndex = 0;
-
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-    })
-})
