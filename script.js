e369f211ea585db3790bd67c00882a3948b8d5ef
Lpik97 <Lpik97@users.noreply.github.com>
2023-04-29 07:56:52 +0200

    A new eventlistener waiting for a scroll was placed in the scrip, this will wait until the scrollY value is equal or greater to 180 to then add the scrolled-... to the salon elements.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const navBar = document.querySelector('.navbar');
 const navBarLogo = document.querySelector('.navbar-logo');
 const navBarList = document.querySelector('.navbar-list');
 const hero = document.querySelector('.hero');
+const salonDonQuijote = document.querySelector('.salon-don-quijote');
+const salonDulcinea = document.querySelector('.salon-dulcinea');
+const salonSancho = document.querySelector('.salon-sancho');
+
+
 
 function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
@@ -22,5 +27,13 @@ document.addEventListener('scroll', function () {
         navBarLogo.classList.remove('scrolled-navbar-logo');
         navBarList.classList.remove('scrolled-navbar-list');
         hero.classList.remove('scrolled-hero');
+    }
+});
+
+document.addEventListener('scroll', function () {
+    if (window.scrollY >= 180) {
+        salonDonQuijote.classList.add('scrolled-salon-don-quijote');
+        salonDulcinea.classList.add('scrolled-salon-dulcinea');
+        salonSancho.classList.add('scrolled-salon-sancho');
     }
 });
