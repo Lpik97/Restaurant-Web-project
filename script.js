5fa89c2a83a532d12f788e2c262757b52716bf93
Lpik97 <Lpik97@users.noreply.github.com>
2023-05-09 17:21:15 +0200

    feat(new function to open new tabs)
    A new function to open a new tab was added for the clients that want to have a budget.

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function openWhatsApp () {
     window.open('https://api.whatsapp.com/send/?phone=541152630497&text=Quisiera+obtener+mas+información+acerca+de+los+salones.&type=phone_number&app_absent=0', 'new-window', 'height=600,width=800,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=yes,directories=yes,status=yes');
 }
 
+function openNewTab () {
+    window.open('https://presupuesto.donquijoterecepciones.com/', 'new-window');
+}
+
 function reloadPage () {
     window.scrollTo(0, 0);
     window.location.reload();
