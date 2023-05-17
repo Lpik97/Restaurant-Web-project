096fa8e71798fdc5fe1b80c8d431dd690aa847df
Lpik97 <Lpik97@users.noreply.github.com>
2023-05-17 14:41:54 +0200

    feat(Carousel Buttons click event listener)
    The carousel buttons are selected and then used in a forEach method waiting for a click on any of them. When a button is clicked, it determines the direction of the carousel based on the button's data attribute, finds the current active slide, calculates the index of the next slide, and updates all in once.

--- script.js
+++ script.js
@@ -109,6 +109,22 @@ salones.forEach(salon => {
                   <ul data-slides>
                   </ul>
                 </div>
+                <script>
+                const carouselButtons = document.querySelectorAll("[data-carousel-button]");
+                carouselButtons.forEach(button => {
+                    button.addEventListener("click", () => {
+                        const offset = button.dataset.carouselButton === 'right' ? 1 : -1;
+                        const slides = button.closest('[data-carousel]').querySelector('[data-slides]');
+                        const activeSlide = slides.querySelector('[data-active]');
+                        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+                        if (newIndex < 0) newIndex = slides.children.length - 1;
+                        if (newIndex >= slides.children.length) newIndex = 0;
+                
+                        slides.children[newIndex].dataset.active = true;
+                        delete activeSlide.dataset.active;
+                    })
+                })
+                </script>
               </body>
             </html>
           `);
